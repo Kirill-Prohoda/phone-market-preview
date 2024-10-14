2f8d9ccb469d3c22caa5f6a03e029c6f292414af
Вячеслав Прохода <Wjascheslaw88@gmail.com>
2024-10-14 12:05:14 +0400

    PHO-5 Исправил недочеты

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
-import './App.css';
+import classes from './App.module.css';
+import Catalog from './pages/catalog/Catalog';
 import Header from './pages/header/Header';
+
 
 function App() {
   return (
     <div>
       <Header/>
+      <div className={classes.content}> 
+      <Catalog/>
+      </div>
     </div>
   );
 }

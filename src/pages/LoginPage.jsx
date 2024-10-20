80a9e3b128053e8a6ffd1d4bb732c67ba5a0ca47
Вячеслав Прохода <Wjascheslaw88@gmail.com>
2024-10-20 10:47:46 +0400

    PHO-11 Закончили

--- src/pages/LoginPage.jsx
+++ src/pages/LoginPage.jsx
@@ -1,9 +1,42 @@
-import React from "react";
+import React, { useState } from "react";
 import classes from './LoginPage.module.css'
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
 
 
+const ADMIN = 'admin'
+const PASSWORD = '1234'
 
 const LoginPage = () => {
+
+
+    const [email, setEmail] = useState('')
+
+    const hendEmail = (d) => {
+        setEmail(d.target.value)
+    }
+
+    const [password, setPassword] = useState('')
+
+    const hendPassword = (d) => {
+        setPassword(d.target.value)
+
+    }
+
+    function toLink() {
+        window.location.assign('http://localhost:3000/home')
+    }
+
+    let notRightEmail = true
+    if (email === ADMIN) {
+        notRightEmail = false
+    }
+
+    let notRightPassword = true
+    if (password === PASSWORD) {
+        notRightPassword = false
+    }
+
+    let isDisabled = notRightEmail || notRightPassword
 
     return (
         <div className={classes.wrapper}>
@@ -12,25 +45,25 @@ const LoginPage = () => {
 
                 </div>
                 <div className={classes.window}>
-                   <div >
-                    <p className={classes.email}>E-mail</p>
-                    <input className={classes.inEmail}/>
+                    <div >
+                        <p className={classes.email}>E-mail</p>
+                        <input value={email} onChange={hendEmail} className={classes.inEmail} />
                     </div>
 
-                   <div>
-                    <p className={classes.password}>Password</p>
-                    <input className={classes.inPassword}/>
+                    <div>
+                        <p className={classes.password}>Password</p>
+                        <input value={password} onChange={hendPassword} className={classes.inPassword} />
                     </div>
-                  
+
                 </div>
-<div className={classes.buttons}>
-    <button className={classes.create}>
-CREATE ACCOUNT
-    </button>
-    <button className={classes.sign}>
-SIGN IN
-    </button>
-</div>
+                <div className={classes.buttons}>
+                    <button className={classes.create}>
+                        CREATE ACCOUNT
+                    </button>
+                    <button disabled={isDisabled} onClick={toLink} className={classes.sign + ' ' + (isDisabled ? classes.sign__disabled : '')}>
+                        SIGN IN
+                    </button>
+                </div>
             </div>
         </div>
     )
@@ -38,3 +71,5 @@ SIGN IN
 
 
 export default LoginPage;
+
+

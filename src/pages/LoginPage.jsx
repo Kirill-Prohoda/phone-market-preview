08675c771fbbf2866354c01e956df609500467d8
Вячеслав Прохода <Wjascheslaw88@gmail.com>
2024-10-21 10:54:35 +0400

    PHO-12 Создал страницу регистрации

--- src/pages/LoginPage.jsx
+++ src/pages/LoginPage.jsx
@@ -13,64 +13,65 @@ const LoginPage = () => {
   const hendPassword = (d) => {
     setPassword(d.target.value);
   };
-  
-    function toLinkM() {
-        window.location.assign('http://localhost:3000/home')
-    }
 
-    function toLinkP() {
-        window.location.assign('http://localhost:3000/registration')
-    }
+  function toLinkM() {
+    window.location.assign("http://localhost:3000/home");
+  }
 
- const toLogin = () => {
-      fetch('http://localhost:4000/login', {
-            method:'POST',
-            headers:{ "Content-Type": "application/json"},
-            body: JSON.stringify({
-            login: email,
-            pass: password,
-            })
-    
-            })
-            .then((data) => data.json())
+  function toLinkP() {
+    window.location.assign("http://localhost:3000/registration");
+  }
 
-            .then((data) => {
-             if(data.status){
-               toLinkM()
-             }
+  const toLogin = () => {
+    fetch("http://localhost:4000/login", {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({
+        login: email,
+        pass: password,
+      }),
+    })
+      .then((data) => data.json())
+      .then((data) => {
+        if (data.status) {
+          toLinkM();
+        }
+      });
+  };
 
-            })
-        
-}
-    return (
-        <div className={classes.wrapper}>
-            <div className={classes.container}>
-                <div className={classes.avatar}>
+  return (
+    <div className={classes.wrapper}>
+      <div className={classes.container}>
+        <div className={classes.avatar}></div>
+        <div className={classes.window}>
+          <div>
+            <p className={classes.email}>E-mail</p>
+            <input
+              value={email}
+              onChange={hendEmail}
+              className={classes.inEmail}
+            />
+          </div>
 
-                </div>
-                <div className={classes.window}>
-                    <div >
-                        <p className={classes.email}>E-mail</p>
-                        <input value={email} onChange={hendEmail} className={classes.inEmail} />
-                    </div>
-
-                    <div>
-                        <p className={classes.password}>Password</p>
-                        <input value={password} onChange={hendPassword} className={classes.inPassword} />
-                    </div>
-
-                </div>
-                <div className={classes.buttons}>
-                    <button onClick={toLinkP} className={classes.create}>
-                        CREATE ACCOUNT
-                    </button>
-                    <button  onClick={toLinkM} className={classes.sign}>
-                        SIGN IN
-                    </button>
-                </div>
-            </div>
+          <div>
+            <p className={classes.password}>Password</p>
+            <input
+              value={password}
+              onChange={hendPassword}
+              className={classes.inPassword}
+            />
+          </div>
         </div>
-     
+        <div className={classes.buttons}>
+          <button onClick={toLinkP} className={classes.create}>
+            CREATE ACCOUNT
+          </button>
+          <button onClick={toLogin} className={classes.sign}>
+            SIGN IN
+          </button>
+        </div>
+      </div>
+    </div>
   );
 };
 
--- src/pages/RegPege.jsx
+++ src/pages/RegPege.jsx
@@ -1,101 +1,92 @@
 import React, { useState } from "react";
-import classes from './RegPage.module.css'
-
-
-
-const ADMIN = 'admin'
-const PASSWORD = '1234'
+import classes from "./RegPage.module.css";
 
 const RegPage = () => {
+  const [email, setEmail] = useState("");
 
+  const hendEmail = (d) => {
+    setEmail(d.target.value);
+  };
 
-    const [email, setEmail] = useState('')
+  const [password, setPassword] = useState("");
 
-    const hendEmail = (d) => {
-        setEmail(d.target.value)
-    }
+  const hendPassword = (d) => {
+    setPassword(d.target.value);
+  };
 
-    const [password, setPassword] = useState('')
+  const [age, setAge] = useState("");
 
-    const hendPassword = (d) => {
-        setPassword(d.target.value)
+  const hendAge = (d) => {
+    setAge(d.target.value);
+  };
 
-    }
+  function toLink() {
+    window.location.assign("http://localhost:3000/");
+  }
 
-    const [age, setAge] = useState('')
+  const toReg = () => {
+    fetch("http://localhost:4000/reg", {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
 
-    const hendAge = (d) => {
-        setAge(d.target.value)
-       
-    }
-    function toLink() {
-        window.location.assign('http://localhost:3000/')
-    }
-
-<<<<<<< HEAD
-  
-=======
-   const toReg = async () => {
-  await  fetch('http://localhost:4000/reg', {
-        method:'POST',
-        headers:{ "Content-Type": "application/json"},
-
-        body: JSON.stringify({
+      body: JSON.stringify({
         login: email,
         pass: password,
-        age: age, 
-        })
-
-        })
-        .then((data) => data.json())
-        .then((data) => {
-           if(data.status){
-            toLink()
-          }
-        
+        age: age,
+      }),
     })
-    
+      .then((data) => data.json())
+      .then((data) => {
+        if (data.status) {
+          toLink();
+        }
+      });
+  };
 
+  return (
+    <div className={classes.wrapper}>
+      <div className={classes.container}>
+        <div className={classes.window}>
+          <div>
+            <p className={classes.email}>E-mail</p>
+            <input
+              value={email}
+              onChange={hendEmail}
+              className={classes.inEmail}
+            />
+          </div>
 
-   }
->>>>>>> e8d746e (PHO-12 Создал регистрацию)
-    return (
-        <div className={classes.wrapper}>
-            <div className={classes.container}>
-                <div className={classes.window}>
-                    <div >
-                        <p className={classes.email}>E-mail</p>
-                        <input value={email} onChange={hendEmail} className={classes.inEmail} />
-                    </div>
+          <div>
+            <p className={classes.Age}>Age</p>
+            <input
+              type="number"
+              value={age}
+              onChange={hendAge}
+              className={classes.inAge}
+            />
+          </div>
 
-                    <div>
-                        <p className={classes.Age}>Age</p>
-                        <input type="number" value={age} onChange={hendAge} className={classes.inAge}/>
-                    </div>
-
-                    <div>
-                        <p className={classes.password}>Password</p>
-                        <input value={password} onChange={hendPassword} className={classes.inPassword} />
-                    </div>
-
-                </div>
-
-                <div className={classes.buttons}>
-<<<<<<< HEAD
-                    <button  onClick={toLink} className={classes.confirm}>
-=======
-                    <button  onClick={toLink} className={classes.login}>
-                    login
-                    </button>
-                    <button  onClick={toReg} className={classes.confirm}>
->>>>>>> e8d746e (PHO-12 Создал регистрацию)
-                    CONFIRM
-                    </button>
-                </div>
-            </div>
+          <div>
+            <p className={classes.password}>Password</p>
+            <input
+              value={password}
+              onChange={hendPassword}
+              className={classes.inPassword}
+            />
+          </div>
         </div>
-    )
-}
 
+        <div className={classes.buttons}>
+          <button onClick={toLink} className={classes.login}>
+            login
+          </button>
+          <button onClick={toReg} className={classes.confirm}>
+            CONFIRM
+          </button>
+        </div>
+      </div>
+    </div>
+  );
+};
 
 export default RegPage;

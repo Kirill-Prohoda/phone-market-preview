489a48d6b96a2365babf5ae72e9ddaeef9273ee7
Вячеслав Прохода <Wjascheslaw88@gmail.com>
2024-10-24 11:42:46 +0400

    PHO-12 Починили

--- server/server.js
+++ server/server.js
@@ -50,6 +50,15 @@ if (user.age === '' || user.login === '' || user.pass === '') {
 
 
 
+if (user.age === '' || user.login === '' || user.pass === '') {
+  return res.status(200).json({
+    status: false,
+    message: "Заполните поля",
+  });
+}
+
+
+
   if (users.find((user) => user.login === login)) {
     return res.status(200).json({
       status: false,
